5c8fb0eeb56368b104505144e046685ec4058dba
Fanis Papoutsidis <thpapoutsidis@gmail.com>
2026-06-19 22:22:40 +0300

    !

--- js/main.js
+++ js/main.js
@@ -4,14 +4,17 @@
 (function($) {
   "use strict";
 
-  // 1. Preloader fadeout
+  // 1. Preloader fadeout (Splash Screen Animation)
   $(window).on('load', function() {
     var loader = $('#ftco-loader');
     if (loader.length > 0) {
-      loader.addClass('hide');
+      // Delay preloader removal for 2.0 seconds to play the SVG drawing/fade-in animation fully
       setTimeout(function() {
-        loader.remove();
-      }, 500);
+        loader.addClass('hide');
+        setTimeout(function() {
+          loader.remove();
+        }, 600);
+      }, 2000);
     }
   });
 
@@ -53,25 +56,6 @@
         });
       }
 
-      var testCarousel = $('.carousel-testimony');
-      if (testCarousel.length > 0) {
-        testCarousel.owlCarousel({
-          autoplay: true,
-          autoHeight: true,
-          center: true,
-          loop: true,
-          items: 1,
-          margin: 30,
-          stagePadding: 0,
-          nav: false,
-          dots: true,
-          responsive: {
-            0: { items: 1 },
-            600: { items: 2 },
-            1000: { items: 3 }
-          }
-        });
-      }
     };
     
     // Call carousel init if owlCarousel function is defined
@@ -79,17 +63,6 @@
       carouselInit();
     }
 
-    // 4. Stellar Parallax initialization
-    if (typeof $.fn.stellar === 'function') {
-      $(window).stellar({
-        responsive: true,
-        parallaxBackgrounds: true,
-        parallaxElements: true,
-        horizontalScrolling: false,
-        hideDistantElements: false,
-        scrollProperty: 'scroll'
-      });
-    }
 
     // 5. Smooth Scroll navigation
     $('.navbar-nav a[href^="#"], .btn[href^="#"]').on('click', function(e) {
@@ -130,30 +103,6 @@
       });
     });
 
-    // 7. Counter animation (Waypoint + animateNumber)
-    var counterInit = function() {
-      var counterSection = $('#section-counter, .ftco-counter');
-      if (counterSection.length > 0 && typeof $.fn.waypoint === 'function') {
-        counterSection.waypoint(function(direction) {
-          if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
-            var comma_separator_number_step = $.animateNumber.numberStepFactor.separator(',');
-            $('.number').each(function() {
-              var $this = $(this),
-                  num = $this.data('number');
-              $this.animateNumber({
-                number: num,
-                numberStep: comma_separator_number_step
-              }, 7000);
-            });
-            $(this.element).addClass('ftco-animated');
-          }
-        }, { offset: '95%' });
-      }
-    };
-    
-    if (typeof $.fn.animateNumber === 'function') {
-      counterInit();
-    }
   });
 
 })(jQuery);
