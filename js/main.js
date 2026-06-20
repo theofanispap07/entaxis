cc0cb82649a09c7256a25dd37ed413edea3422ce
Fanis Papoutsidis <thpapoutsidis@gmail.com>
2026-06-20 10:38:08 +0300

    !

--- js/main.js
+++ js/main.js
@@ -4,18 +4,52 @@
 (function($) {
   "use strict";
 
-  // 1. Preloader fadeout (Splash Screen Animation)
-  $(window).on('load', function() {
-    var loader = $('#ftco-loader');
-    if (loader.length > 0) {
-      // Delay preloader removal for 2.0 seconds to play the SVG drawing/fade-in animation fully
-      setTimeout(function() {
-        loader.addClass('hide');
-        setTimeout(function() {
-          loader.remove();
-        }, 600);
-      }, 2000);
+  // 1. Preloader fadeout (Splash Screen Animation) with Typewriter Effect
+  var splashText = "Ev Taxis";
+  var currentText = "";
+  var charIndex = 0;
+  var typingSpeed = 70; // ms per character
+  var isTypingComplete = false;
+  var windowLoaded = false;
+
+  function typeNextChar() {
+    var typedElement = $('#splash-logo-typed');
+    if (typedElement.length > 0 && charIndex < splashText.length) {
+      currentText += splashText.charAt(charIndex);
+      typedElement.text(currentText);
+      charIndex++;
+      setTimeout(typeNextChar, typingSpeed);
+    } else {
+      isTypingComplete = true;
+      $(window).trigger('splash:typed');
     }
+  }
+
+  // Start typing after a short initial delay for the logo icon to scale in
+  setTimeout(typeNextChar, 150);
+
+  function checkDismissSplash() {
+    if (windowLoaded && isTypingComplete) {
+      var loader = $('#ftco-loader');
+      if (loader.length > 0) {
+        // Minimal delay after typing finishes to let the final state linger briefly
+        setTimeout(function() {
+          loader.addClass('hide');
+          setTimeout(function() {
+            loader.remove();
+          }, 400); // Quick fade out transition
+        }, 150);
+      }
+    }
+  }
+
+  $(window).on('load', function() {
+    windowLoaded = true;
+    checkDismissSplash();
+  });
+
+  $(window).on('splash:typed', function() {
+    checkDismissSplash();
   });
 
   $(document).ready(function() {
